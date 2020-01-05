0bd6ce41d60b03f26e9f7fa109e3220d095a32a3
tony84727 <tony84727@gmail.com>
2020-01-05 22:22:16 +0800

    replace ExampleMod

---
+++ src/main/java/com/github/tony84727/unrealuniverse/UnrealUniverse.java
@@ -0,0 +1,33 @@
+package com.github.tony84727.unrealuniverse;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("unrealuniverse")
+public class UnrealUniverse {
+    public UnrealUniverse() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onItemRegistry(final RegistryEvent.Register<Item> itemRegistryEvent) {
+        }
+
+        @SubscribeEvent
+        public static void onBlockRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+
+        }
+    }
+}
